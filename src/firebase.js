5191450dbe2d6181c61ae8dfdc54655a6e158a24
JonLyn <jonlyn22@gmail.com>
2021-02-19 16:42:08 -0500

    added env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase/app';
+import 'firebase/auth'
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5yKaZnAfLiXauKrMcaIavutiSwzmhv6A",
-  authDomain: "aisle-attack.firebaseapp.com",
-  projectId: "aisle-attack",
-  storageBucket: "aisle-attack.appspot.com",
-  messagingSenderId: "234733929900",
-  appId: "1:234733929900:web:06c65f8b11d8427ceda609"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
